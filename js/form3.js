c863758680904423f2855302a2e69dbc86319f9d
Alex Turnbull <A.Turnbull@kainos.com>
2015-10-21 16:01:44 +0100

    added the form3 code

---
+++ js/form3.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+	$.getJSON('/data.json', function(data) {
+		$.each(data.touristdata.attractions, function(index, value) {
+		   $('#attractions').append("<a onclick='goToDetail(" + value.id + ")' style='color:inherit' href='attractiondetail.html'><li style='list-style:none' class='collection-item avatar'><img class = 'circle' src = '" + value.photo + "' /><span class='title'>" + value.name + "</span><p>" + value.address + "</p></li></a>");
+		});
+
+ 	 });
+});
+
+
+function goToDetail(id) {
+  localStorage.setItem('id', id);
+  return true;
+}
